d151a3f96bc8a3a5db4981186c280887dd75b583
Nugroho Adi Pramono <nugnux@gmail.com>
2024-02-21 14:05:51 +0700

    Update slider.py

--- slider.py
+++ slider.py
@@ -20,3 +20,19 @@ ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
 ax.tick_params(axis='x', labelsize=15)
 plt.grid(color='green', linestyle='-.', linewidth=.5)
 st.pyplot(fig)
+
+def f(x):
+  return 14*x**2+21*x-7
+
+u = f(t)
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(t, u, label='sin(t)', color='b')  # Plotting sin(t) curve
+ax.set_ylabel("")
+ax.set_xlabel("t")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
+plt.grid(color='green', linestyle='-.', linewidth=.5)
+st.pyplot(fig)
+
+
