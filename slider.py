019bb3c31ba9beda168239af13a47284e10e3694
Nugroho Adi Pramono <nugnux@gmail.com>
2024-02-21 11:36:20 +0700

    Update slider.py

--- slider.py
+++ slider.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import numpy as np
+import matplotlib as plt
 
-x = st.slider(
-    'Pilih rentang',
-    0.0, 2.0, (.2, .5))
+x = st.slider('Pilih rentang', 0.0, 2.0, (.2, .5))
 st.write('nilai x:', x)
-y = st.slider(
-    'Set nilai',
-    0.0, 100.0, 25.0)
+y = st.slider('Set nilai',0.0, 100.0, 25.0)
 st.write('nilai y:', y)
+
+t = np.linspace(x[0],x[1],100)
+u = np.sin(t)
