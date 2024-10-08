23f70aad904f2b2e29e5b390918fab868684dbca
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-08 11:32:27 +0700

    Update avo.py

--- avo.py
+++ avo.py
@@ -2,15 +2,21 @@ import streamlit as st
 import random
 
 st.title("Alat Ukur Tak Presisi")
+
+#AVO
 st.header("Amperemeter", divider=True)
 
 r = st.slider('R : ',1., 4700., 1., .1)
 st.write('R:', r, 'Ohm')
 v = st.slider('V ',0.0, 12., 0.)
-#st.write('V:', v, ' Volt')
-i = v/(r + random.random() - .5)
+st.write('V:', v, ' Volt')
+i = v/(r + random.random()/10 - .5)
 st.write('I=', i,'A =',i/1000,'mA')
 
+#Ayunan
 st.header("Ayunan", divider=True)
+
+l = st.slider('Panjang tali : ',1., 57., 1., .1)
+st.write('l:', r, 'cm')
 
 
