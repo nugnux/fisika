13d626da0e9b6a0b87272cafb621b4f81a52bbe6
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-08 11:30:19 +0700

    Update avo.py

--- avo.py
+++ avo.py
@@ -4,13 +4,13 @@ import random
 st.title("Alat Ukur Tak Presisi")
 st.header("Amperemeter", divider=True)
 
-
-r = st.slider('R = ',1., 4700., 1., .1)
-#st.write('R:', r)
+r = st.slider('R : ',1., 4700., 1., .1)
+st.write('R:', r, 'Ohm')
 v = st.slider('V ',0.0, 12., 0.)
-#st.write('V:', v)
+#st.write('V:', v, ' Volt')
 i = v/(r + random.random() - .5)
-st.write('I:', i)
+st.write('I=', i,'A =',i/1000,'mA')
 
 st.header("Ayunan", divider=True)
+
 
