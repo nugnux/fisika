8e25d0c8503335be970bb88b584d99d7cfc3e996
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-08 13:21:00 +0700

    Update avo.py

--- avo.py
+++ avo.py
@@ -37,6 +37,35 @@ with pegas:
   tp = 2*np.pi*np.sqrt((m/1000)/(k+np.random.random()-.5))
   st.write('Periode Ayunan Pegas =', tp,' s = ',1000*tp,'ms')
 
+import time
+import numpy as np
+import pandas as pd
+
+_LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
+
+
+def stream_data():
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+    yield pd.DataFrame(
+        np.random.randn(5, 10),
+        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+    )
+
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+
+if st.button("Stream data"):
+    st.write_stream(stream_data)
+
 
 
 
