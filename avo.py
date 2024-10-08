52f71a57b5ddda9a502595ea39abfb27e8f8b26e
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-08 11:26:02 +0700

    Update avo.py

--- avo.py
+++ avo.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import random
 
+st.title("Alat Ukur Tak Presisi")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+
 r = st.slider('R = ',1., 4700., 1., .1)
 #st.write('R:', r)
 v = st.slider('V ',0.0, 12., 0.)
