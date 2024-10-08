b88d7698ec98d1c3fa6267ce2190616101c265f0
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-08 11:43:36 +0700

    Update avo.py

--- avo.py
+++ avo.py
@@ -12,7 +12,7 @@ st.write('R:', r, 'Ohm')
 v = st.slider('V ',0.0, 12., 0.)
 st.write('V:', v, ' Volt')
 i = v/(r + random.random() - .5)
-st.write('I=', i,'A =',i/1000,'mA')
+st.write('I=', i,'A =',i*1000,'mA')
 
 #Ayunan
 st.header("Ayunan", divider=True)
