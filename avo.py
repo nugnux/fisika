4800d0e4dcc561d3e287163034c3f2dab626e3d3
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-08 11:28:29 +0700

    Update avo.py

--- avo.py
+++ avo.py
@@ -2,7 +2,8 @@ import streamlit as st
 import random
 
 st.title("Alat Ukur Tak Presisi")
-st.title("Amperemeter")
+st.header("Amperemeter", divider=True)
+
 
 r = st.slider('R = ',1., 4700., 1., .1)
 #st.write('R:', r)
@@ -10,4 +11,6 @@ v = st.slider('V ',0.0, 12., 0.)
 #st.write('V:', v)
 i = v/(r + random.random() - .5)
 st.write('I:', i)
+
+st.header("Ayunan", divider=True)
 
