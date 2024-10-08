04de089b28f7d68acb3d4130d48c9029f6276c79
Nugroho Adi Pramono <nugnux@gmail.com>
2024-10-08 10:42:59 +0700

    Update avo.py

--- avo.py
+++ avo.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-r = st.slider('R = ', -7.0, 7.0, (.2, .5))
+r = st.slider('R = ', 1.0, 4700.0, (.2, .5))
 st.write('R:', r)
 v = st.slider('V ',0.0, 12, .1)
 st.write('V:', y)
