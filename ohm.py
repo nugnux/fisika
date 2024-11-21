8f9846774198839b4be6a5048f1c978003124097
Nugroho Adi Pramono <nugnux@gmail.com>
2024-11-21 14:50:40 +0700

    Create ohm.py

---
+++ ohm.py
@@ -0,0 +1,20 @@
+
+st.title("Alat Ukur Tak Presisi")
+import streamlit as st
+import random
+import numpy as np
+
+
+st.title("Alat Ukur Tak Presisi")
+
+#AVO
+st.header("Amperemeter", divider=True)
+avo =  st.container(border=True)
+with avo:
+  v = st.slider('V ',0.0, 12., 0.)
+  st.write('V:', v, ' Volt')
+  i = (v + random.random() - .5)/(371)
+  st.write('I =', i,'A =',i*1000,'mA')
+
+
+st.header("Ayunan Bandul", divider=True)
