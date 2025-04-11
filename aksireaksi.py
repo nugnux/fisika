1ff7606a417e867611053eb26c62194d5fd39e14
Nugroho Adi Pramono <nugnux@gmail.com>
2025-04-11 20:11:25 +0700

    Update aksireaksi.py

--- aksireaksi.py
+++ aksireaksi.py
@@ -6,5 +6,5 @@ st.header('Aksi Reaksi :sparkles:')
 st.subheader('Nugroho')
 
 
-stb.single_choice("Gerak jatuh bebas merupakan salah satu contoh aksi-reaksi",
+x = stb.single_choice("Gerak jatuh bebas merupakan salah satu contoh aksi-reaksi",
                   ["Benar", "Salah"], 1)
