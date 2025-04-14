ba35c3cf3fa32f05957925ed4db5f766a2208480
Nugroho Adi Pramono <nugnux@gmail.com>
2025-04-14 21:53:27 +0700

    Update aksireaksi.py

--- aksireaksi.py
+++ aksireaksi.py
@@ -4,6 +4,11 @@ import streamlit_book as stb
 # Header
 st.header('Aksi Reaksi :sparkles:')
 st.subheader('Nugroho')
+#nilai
+if 'nilai' not in st.session_state:
+    st.session_state['nilai'] = 0
+
+st.write('nilai = ', st.session_state.nilai)
 
 x = stb.single_choice("Gerak jatuh bebas merupakan salah satu contoh aksi-reaksi", ["Benar", "Salah"], 1)
 st.write(x)
@@ -11,16 +16,7 @@ st.write(x)
 if (x[0]):
   if (x[1]):
     st.write("Jawaban Benar")
-    nilai += 1
-    st.query_params(s_nilai = nilai)
+    st.session_state['nilai'] += 1
   else:
     y = stb.single_choice("Aksi reaksi bekerja pada satu benda", ["Benar", "Salah"], 1)
-app_state = st.query_params()  
-st.write(app_state)
-if "s_nilai" in app_state:
-    nnilai = app_state["s_nilai"][0]
-    st.write("Here is your result", nilai)
-else:
-    st.write("No result to display, compute a value first.")
-st.write(nilai)
 
