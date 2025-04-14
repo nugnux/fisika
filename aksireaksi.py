acd8ba64bf902270bd9fb5b4ddf5567d188f9048
Nugroho Adi Pramono <nugnux@gmail.com>
2025-04-14 15:52:49 +0700

    Update aksireaksi.py

--- aksireaksi.py
+++ aksireaksi.py
@@ -4,7 +4,6 @@ import streamlit_book as stb
 # Header
 st.header('Aksi Reaksi :sparkles:')
 st.subheader('Nugroho')
-nilai = 0
 
 x = stb.single_choice("Gerak jatuh bebas merupakan salah satu contoh aksi-reaksi", ["Benar", "Salah"], 1)
 st.write(x)
