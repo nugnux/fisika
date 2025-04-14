a94c8f51f20d4b445358c1ac552afd03b7b98589
Nugroho Adi Pramono <nugnux@gmail.com>
2025-04-14 23:07:12 +0700

    Update aksireaksi.py

--- aksireaksi.py
+++ aksireaksi.py
@@ -14,9 +14,9 @@ for i in range (3):
     if 'c'+str(i)  not in st.session_state:
         st.session_state['c'+str(i)] = 0
 
-x = stb.single_choice("Gerak jatuh bebas merupakan salah satu contoh aksi-reaksi", 
+x1 = stb.single_choice("Gerak jatuh bebas merupakan salah satu contoh aksi-reaksi", 
                       ["Benar", "Salah"], 1)
-st.write(x)
+st.write(x1)
 
 if (x[0]):
   st.session_state.c1 = 1
@@ -29,6 +29,18 @@ if (x[0]):
 
     
 if (st.session_state.s1 == 0 and st.session_state.c1 == 1 ):
-    y = stb.single_choice("Aksi reaksi bekerja pada satu benda", ["Benar", "Salah"], 1)
+    y1 = stb.single_choice("Aksi reaksi bekerja pada satu benda", ["Benar", "Salah"], 1)
+    if (y1[0]):
+      st.session_state.c1 = 1
+      if (y1[1]):
+        st.write("Jawaban Benar")
+        st.session_state['nilai'] += 1
+        st.session_state.s1 = .5
+      else:
+        st.session_state.s1 = 0
 
-st.write('Nilai = ', st.session_state.nilai)
+nil = 0
+for i in range(3):
+    nil += st.session_state['s'+str(i)]
+st.write('Nilai Total= ', nil)
+st.write('Nilai Total= ', st.session_state.nilai)
