f6ca13e0ebd6f1fb1a539aaa79d0050df60beeeb
Nugroho Adi Pramono <nugnux@gmail.com>
2025-04-14 14:50:53 +0700

    Update aksireaksi.py

--- aksireaksi.py
+++ aksireaksi.py
@@ -13,8 +13,15 @@ if (x[0]):
   if (x[1]):
     st.write("Jawaban Benar")
     nilai += 1
+    st.experimental_set_query_params(s_nilai = nilai)
   else:
     y = stb.single_choice("Aksi reaksi bekerja pada satu benda", ["Benar", "Salah"], 1)
-
+app_state = st.experimental_get_query_params()  
+st.write(app_state)
+f "s_nilai" in app_state:
+    nnilai = app_state["s_nilai"][0]
+    st.write("Here is your result", nilai)
+else:
+    st.write("No result to display, compute a value first.")
 st.write(nilai)
 
