ee75119611c6c0e155f52df8b8a145a2076ced6c
Nugroho Adi Pramono <nugnux@gmail.com>
2025-04-14 22:06:58 +0700

    Update aksireaksi.py

--- aksireaksi.py
+++ aksireaksi.py
@@ -7,16 +7,21 @@ st.subheader('Nugroho')
 #nilai
 if 'nilai' not in st.session_state:
     st.session_state['nilai'] = 0
+for i in range (3):
+    if 's'+str(i)  not in st.session_state:
+        st.session_state['s'+str(i)] = 0
 
-st.write('nilai = ', st.session_state.nilai)
-
-x = stb.single_choice("Gerak jatuh bebas merupakan salah satu contoh aksi-reaksi", ["Benar", "Salah"], 1)
+x = stb.single_choice("Gerak jatuh bebas merupakan salah satu contoh aksi-reaksi", 
+                      ["Benar", "Salah"], 1)
 st.write(x)
 
 if (x[0]):
   if (x[1]):
     st.write("Jawaban Benar")
     st.session_state['nilai'] += 1
-  else:
+    st.session_state.s1 = 1
+    
+if (!st.session_state.s1):
     y = stb.single_choice("Aksi reaksi bekerja pada satu benda", ["Benar", "Salah"], 1)
 
+st.write('Nilai = ', st.session_state.nilai)
