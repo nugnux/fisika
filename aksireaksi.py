57cc464e7c04f404a7b0b2d735247ba4ebc27557
Nugroho Adi Pramono <nugnux@gmail.com>
2025-04-16 00:33:24 +0700

    Update aksireaksi.py

--- aksireaksi.py
+++ aksireaksi.py
@@ -17,9 +17,8 @@ for i in range (3):
 x1 = stb.single_choice("Sebuah benda bermassa 0,8 kg digantung pada pegas vertikal dengan konstanta k = 160 N/m. Benda digetarkan dari posisi setimbang.", 
         ["0,32 s", "0,44 s", "0,56 s", "0,62 s"], 2,
                        success='Benar, $ T = 2 \pi \sqrt{\\frac{0.8}{160}} \\approx 0.56 \,s$', 
-                       error='Pastikan rumus yang anda gunakan benar, jangna lupa tanda akar.')
+                       error='Pastikan rumus yang anda gunakan benar, jangan lupa tanda akar.')
 st.write(x1)
-st.latex(" T = 2 \pi \sqrt{\\frac{0.8}{160}} \\approx 0.56 \,s")
 
 if (x1[0]):
   st.session_state.c1 = 1
